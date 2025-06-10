b143202142c1f040eb24cb72ce1fec8de45a2ec2
muhammedozdemir23 <muhammedozdemr3@gmail.com>
2025-06-10 19:35:58 +0300

    ogrencisil parametre

---
+++ OgrenciKayitSistemi.Application/DTOs/Params/OgrenciSilPar.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciKayitSistemi.Application.DTOs.Params
+{
+    public class OgrenciSilPar
+    {
+        public long ogrenciId { get; set; }
+
+        public long sinifId { get; set; }
+
+    }
+}
